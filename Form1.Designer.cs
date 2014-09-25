f3fd270366c20308cc6c0c10d37199606e7f25fe
tstaten56 <tstaten56@gmail.com>
2014-09-25 10:28:39 -0400

    A simple windows service I made using tutorials.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace SampleServiceUI2
+{
+    partial class ServiceControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startService = new System.Windows.Forms.Button();
+            this.stopService = new System.Windows.Forms.Button();
+            this.pauseService = new System.Windows.Forms.Button();
+            this.currentStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorMsg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.continueService = new System.Windows.Forms.Button();
+            this.leftNum = new System.Windows.Forms.TextBox();
+            this.equals = new System.Windows.Forms.Button();
+            this.rightNum = new System.Windows.Forms.TextBox();
+            this.opSelect = new System.Windows.Forms.ComboBox();
+            this.operationResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startService
+            // 
+            this.startService.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startService.Location = new System.Drawing.Point(86, 67);
+            this.startService.Name = "startService";
+            this.startService.Size = new System.Drawing.Size(113, 23);
+            this.startService.TabIndex = 0;
+            this.startService.Text = "Start Service";
+            this.startService.UseVisualStyleBackColor = true;
+            this.startService.Click += new System.EventHandler(this.startService_Click);
+            // 
+            // stopService
+            // 
+            this.stopService.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stopService.Location = new System.Drawing.Point(86, 184);
+            this.stopService.Name = "stopService";
+            this.stopService.Size = new System.Drawing.Size(113, 23);
+            this.stopService.TabIndex = 1;
+            this.stopService.Text = "Stop Service";
+            this.stopService.UseVisualStyleBackColor = true;
+            this.stopService.Click += new System.EventHandler(this.stopService_Click);
+            // 
+            // pauseService
+            // 
+            this.pauseService.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.pauseService.Location = new System.Drawing.Point(86, 105);
+            this.pauseService.Name = "pauseService";
+            this.pauseService.Size = new System.Drawing.Size(113, 23);
+            this.pauseService.TabIndex = 2;
+            this.pauseService.Text = "Pause Service";
+            this.pauseService.UseVisualStyleBackColor = true;
+            this.pauseService.Click += new System.EventHandler(this.pauseService_Click);
+            // 
+            // currentStatus
+            // 
+            this.currentStatus.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.currentStatus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.currentStatus.Location = new System.Drawing.Point(86, 27);
+            this.currentStatus.Name = "currentStatus";
+            this.currentStatus.ReadOnly = true;
+            this.currentStatus.Size = new System.Drawing.Size(113, 20);
+            this.currentStatus.TabIndex = 3;
+            this.currentStatus.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // errorMsg
+            // 
+            this.errorMsg.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.errorMsg.Location = new System.Drawing.Point(7, 328);
+            this.errorMsg.Multiline = true;
+            this.errorMsg.Name = "errorMsg";
+            this.errorMsg.ReadOnly = true;
+            this.errorMsg.Size = new System.Drawing.Size(265, 73);
+            this.errorMsg.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Errors:";
+            // 
+            // continueService
+            // 
+            this.continueService.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.continueService.Location = new System.Drawing.Point(86, 145);
+            this.continueService.Name = "continueService";
+            this.continueService.Size = new System.Drawing.Size(113, 23);
+            this.continueService.TabIndex = 7;
+            this.continueService.Text = "Continue Service";
+            this.continueService.UseVisualStyleBackColor = true;
+            this.continueService.Click += new System.EventHandler(this.continueService_Click);
+            // 
+            // leftNum
+            // 
+            this.leftNum.BackColor = System.Drawing.Color.YellowGreen;
+            this.leftNum.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftNum.Location = new System.Drawing.Point(33, 230);
+            this.leftNum.Name = "leftNum";
+            this.leftNum.Size = new System.Drawing.Size(51, 20);
+            this.leftNum.TabIndex = 8;
+            // 
+            // equals
+            // 
+            this.equals.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.equals.Location = new System.Drawing.Point(86, 256);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(113, 23);
+            this.equals.TabIndex = 9;
+            this.equals.Text = "Equals";
+            this.equals.UseVisualStyleBackColor = true;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // rightNum
+            // 
+            this.rightNum.BackColor = System.Drawing.Color.YellowGreen;
+            this.rightNum.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightNum.Location = new System.Drawing.Point(90, 230);
+            this.rightNum.Name = "rightNum";
+            this.rightNum.Size = new System.Drawing.Size(56, 20);
+            this.rightNum.TabIndex = 10;
+            // 
+            // opSelect
+            // 
+            this.opSelect.FormattingEnabled = true;
+            this.opSelect.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.opSelect.Location = new System.Drawing.Point(152, 229);
+            this.opSelect.Name = "opSelect";
+            this.opSelect.Size = new System.Drawing.Size(66, 21);
+            this.opSelect.TabIndex = 11;
+            this.opSelect.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // operationResult
+            // 
+            this.operationResult.BackColor = System.Drawing.Color.YellowGreen;
+            this.operationResult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.operationResult.Location = new System.Drawing.Point(114, 285);
+            this.operationResult.Name = "operationResult";
+            this.operationResult.Size = new System.Drawing.Size(56, 20);
+            this.operationResult.TabIndex = 12;
+            // 
+            // ServiceControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(284, 413);
+            this.Controls.Add(this.operationResult);
+            this.Controls.Add(this.opSelect);
+            this.Controls.Add(this.rightNum);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.leftNum);
+            this.Controls.Add(this.continueService);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.errorMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentStatus);
+            this.Controls.Add(this.pauseService);
+            this.Controls.Add(this.stopService);
+            this.Controls.Add(this.startService);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "ServiceControl";
+            this.Text = "Service Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startService;
+        private System.Windows.Forms.Button stopService;
+        private System.Windows.Forms.Button pauseService;
+        private System.Windows.Forms.TextBox currentStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox errorMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button continueService;
+        private System.Windows.Forms.TextBox leftNum;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.TextBox rightNum;
+        private System.Windows.Forms.ComboBox opSelect;
+        private System.Windows.Forms.TextBox operationResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿/*1/10/14
+ * Tera Staten
+ * UI for WindowsService2
+ * Allows user to Start, Stop, and Pause the service window. Also gives 
+ * errors when caught and prints the error to a textbox. Shows the status
+ * of the service in a textbox.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Collections;
+
+
+namespace SampleServiceUI2
+{
+    public partial class ServiceControl : Form
+    {
+        WindowsService2.SampleService add = new WindowsService2.SampleService();//Initialize a SampleService to do addition
+        ServiceController example = new ServiceController("SampleService", "USL-5T43FS1");//Add controller to service name and computer name
+        TimeSpan timeout = TimeSpan.FromMilliseconds(30); //Used for Pause
+        public ServiceControl()
+        {
+            InitializeComponent();
+        }
+
+        private void startService_Click(object sender, EventArgs e)
+        {
+            clearErrorMessage();
+            try
+            {
+                example.Start(); //Starts the service
+                startService.Hide(); //Hiding the Start Service button
+                stopService.Show(); //Showing the Stop Service button 
+                pauseService.Show(); //Showing the Pause Service button
+                continueService.Show(); //Showing the Continue Service button
+                //pauseService.Enabled = false;   ENABLE AND DISABLE BUTTONS LIKE THIS
+            }
+            catch (Exception ex)
+            {
+                errorCaught(ex.Message);
+                Debug.WriteLine("Exception Message: " + ex.Message);
+            }
+            statusChanged("Started");
+        }//End startService_Click
+
+        private void pauseService_Click(object sender, EventArgs e)
+        {
+            clearErrorMessage();
+            try
+            {
+                example.Pause(); //Pauses the service
+                example.WaitForStatus(ServiceControllerStatus.Paused, timeout);
+                statusChanged("Paused");
+                startService.Hide(); //Hiding the Start Service button
+            }
+            catch (Exception ex)
+            {
+                errorCaught(ex.Message);
+                Debug.WriteLine("Exception Message: " + ex.Message);
+            }
+        }//End pauseService_Click
+
+
+        private void continueService_Click(object sender, EventArgs e)
+        {
+            clearErrorMessage();
+            try
+            {
+                example.Continue(); //Continues the service
+                example.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                statusChanged("Continued");
+            }
+            catch (Exception ex)
+            {
+                errorCaught(ex.Message);
+                Debug.WriteLine("Exception Message: " + ex.Message);
+            }
+        }//End continueService_Click
+
+        private void stopService_Click(object sender, EventArgs e)
+        {
+            clearErrorMessage();
+            try
+            {
+                example.Stop(); //Stops the service
+                startService.Show(); //Showing the Start Service button
+                stopService.Hide(); //Hiding the Stop Service button
+                pauseService.Hide(); //Hiding the Pause Service button
+                continueService.Hide(); //Hiding the Continue Service button
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception Message: " + ex.Message);
+                errorCaught(ex.Message);
+            }
+            statusChanged("Stopped");
+        }//End stopService_Click
+
+        private void label1_Click(object sender, EventArgs e)
+        {//Pointless but would not let me delete
+        }//End label1_Click
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {//Pointless but would not let me delete
+        }//End textBox1_TextChanged
+
+        private void clearErrorMessage() //Used to clear the textbox so we can view other errors that may pop up later
+        {
+            errorMsg.Text = "";
+        }//End clearErrorMessage
+
+        private void statusChanged(string msg) //Function for changing the text of the read-only textBox
+        {
+            currentStatus.Text = msg;
+        }//End statusChanged
+
+        private void errorCaught(string msg) //For error message text
+        {
+            errorMsg.Text = msg;
+        }//End errorCaught
+        public List<Hashtable> GetEventEntryByEvent(ref string logName, ref string machineName, ref long instanceId)
+        {
+            try
+            {
+                //Create our list
+                List<Hashtable> events = new List<Hashtable>();
+
+                //Connect to the EventLog of the specified machine
+                EventLog log = new EventLog(logName, machineName);
+
+                //Now we want to loop through each entry
+                foreach (EventLogEntry entry in log.Entries)
+                {
+                    //If we run accross one with the right entry id create a new Hashtable
+                    //then add the Message, InstanceId, Source, and TimeWritten values from that entry
+                    if (entry.InstanceId == instanceId)
+                    {
+                        Hashtable entryInfo = new Hashtable();
+
+                        entryInfo.Add("Message", entry.Message);
+                        entryInfo.Add("InstanceId", entry.InstanceId);
+                        entryInfo.Add("Source", entry.Source);
+                        entryInfo.Add("TimeWritten", entry.TimeWritten);
+
+                        //Add this new hashtable to our list
+                        events.Add(entryInfo);
+
+                        entryInfo = null;
+
+                    }
+                }
+
+                //Return the results
+                return events;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                return null;
+            }
+        } //End GetEventEntryByEvent
+        public bool AddNewEntry(ref string source, ref string logName, ref string message)
+        {
+            try
+            {
+                //First we need to make sure that this source doesn't exist yet
+                if (!EventLog.SourceExists(source))
+                {
+                    //Create our source by providing the source and the name of the 
+                    //new log name
+                    EventLog.CreateEventSource(source, logName);
+
+                    //Due to latency we want to wait a bit for the source and
+                    //log to be created. So we will sleep for 3 seconds
+                    System.Threading.Thread.Sleep(3000);
+                }
+
+                //Now we need a new instance of the EventLog
+                EventLog log = new EventLog();
+                //Set the  source we're writing to
+                log.Source = source;
+
+                //Now write an entry
+                log.WriteEntry(message, EventLogEntryType.Information, 99);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        } //End AddNewEntry
+
+        public bool WriteNewEntry(ref string source, ref string logName, ref string machineName, ref string message)
+        {
+            try
+            {
+                //Create a new instance of the EventLog class
+                EventLog log = new EventLog(logName, machineName, source);
+
+                //Make sure the source exists and the log name exists
+                //if either is found a new exception is thrown and the function is exited
+
+                if (EventLog.SourceExists(source))
+                {
+                    //Make sure the log exists
+                    if (EventLog.Exists(logName))
+                    {
+                        //Write the new entry
+                        log.WriteEntry(message, EventLogEntryType.Information);
+
+                    }
+                    else
+                    {
+                        throw new Exception("Log name specified does not exist!");
+                    }
+                }
+                else
+                {
+                    throw new Exception("The source name provided does not exist!");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }//EndWriteNewEntry
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            double firstNum = convertToDouble(leftNum);
+            double secondNum = convertToDouble(rightNum);
+            Object selectedItem = opSelect.SelectedItem;
+            string op = selectedItem.ToString();
+            switch (op)
+            {
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+            }
+            operationResult.Text = result.ToString();
+        }
+
+        private double convertToDouble(TextBox a)
+        {
+            double num = 0;
+            try
+            {
+                num = Convert.ToDouble(a.Text);
+            }
+            catch (FormatException ex)
+            {
+                errorCaught(ex.Message);
+            }
+            return num;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        } 
+    }//end ServiceControl class
+}//end SampleServiceUI namespace
